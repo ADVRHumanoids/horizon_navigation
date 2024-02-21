b85bef7e4f21d6a6682cc8545089f42e3d9972d4
FrancescoRuscelli <francescoruscelli24@gmail.com>
2024-02-21 19:44:25 +0100

    integration of last commit for class obstacle

--- include/obstacle.h
+++ include/obstacle.h
@@ -13,44 +13,58 @@
 //#include <variant>
 //#include <functional>
 
-class Obstacle {
-public:
+class Obstacle
+{
+    public:
 
-    typedef std::shared_ptr<Obstacle> Ptr;
-    virtual Eigen::Vector3d getOrigin() = 0;
-//    virtual double getDistance() = 0;
-
-};
-
-
-class SphereObstacle {
-public:
-
-    typedef std::shared_ptr<Obstacle> Ptr;
-    virtual Eigen::Vector3d getOrigin() override;
-//    virtual double getDistance() = 0;
+        typedef std::shared_ptr<Obstacle> Ptr;
+        virtual Eigen::Vector3d getOrigin() = 0;
+    //    virtual double getDistance() = 0;
 
 };
 
-class CasadiObstacle: public Obstacle
+
+class SphereObstacle: public Obstacle
+{
+    public:
+
+        typedef std::shared_ptr<SphereObstacle> Ptr;
+        SphereObstacle(Eigen::Vector3d origin, Eigen::Vector3d radius):
+            _origin(origin),
+            _radius(radius) {}
+
+        Eigen::Vector3d getOrigin() override {return _origin; }
+        Eigen::Vector3d getRadius() {return _radius; }
+
+    protected:
+
+        Eigen::Vector3d _origin;
+        Eigen::Vector3d _radius;
+
+};
+
+class CasadiObstacle //: public SphereObstacle
 {
 
 public:
 
     typedef std::shared_ptr<CasadiObstacle> Ptr;
-    CasadiObstacle(Eigen::Vector3d origin, Eigen::Vector3d radius);
+
+    CasadiObstacle();
+    /*Eigen::Vector3d origin, Eigen::Vector3d radius):
+        SphereObstacle(origin, radius) {}*/
 
     casadi::Function gaussFormulation();
     casadi::Function simpleFormulation();
 
-    Eigen::Vector3d getOrigin() override;
-//    double getDistance() override;
-    Eigen::Vector3d getRadius();
+//    Eigen::Vector3d getOrigin();
+////    double getDistance() override;
+//    Eigen::Vector3d getRadius();
 
-private:
+//private:
 
-    Eigen::Vector3d _origin;
-    Eigen::Vector3d _radius;
+//    Eigen::Vector3d _origin;
+//    Eigen::Vector3d _radius;
 
 //    double _distance;
 
