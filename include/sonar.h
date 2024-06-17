a9dc14d8e3b45ca3a59cac123385eaeb22af5a96
FrancescoRuscelli <francescoruscelli24@gmail.com>
2024-06-17 15:16:02 +0200

    added scratch files for sonars

---
+++ include/sonar.h
@@ -0,0 +1,5 @@
+#ifndef SONAR_H
+#define SONAR_H
+
+
+#endif
---
+++ src/sonar_node.cpp
@@ -0,0 +1,21 @@
+#include <ros/ros.h>
+#include <sonar.h>
+
+int main(int argc, char** argv)
+{
+//    ros::init(argc, argv, "sonar_node");
+//    ros::NodeHandle nh(""), nhpr("~");
+
+//    ros::Rate r(rate);
+
+//    while(ros::ok())
+//    {
+
+//        obs_gen._update();
+//        ros::spinOnce();
+//        r.sleep();
+//        iteration++;
+//    }
+
+    return 0;
+}
